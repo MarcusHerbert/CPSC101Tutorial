9865992b84be44a7bf231b90ce83d8296466d5b3
thema <mherbert@unbc.ca>
2026-01-22 09:31:10 -0800

    created branch 3

--- main_module/src/Main.java
+++ main_module/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String[] args) {
         System.out.println("Hello World");
-        System.out.println("This code is from branch 2");
+        System.out.println("This code is from branch 3");
     }
 }
